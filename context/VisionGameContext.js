7ee4cbe4e716472f0ace316e20061f8eb12da41b
Kristy Liu <kristy.liu@plantsist.com>
2024-09-15 22:28:44 +0800

    fix: fix context init data

--- context/VisionGameContext.js
+++ context/VisionGameContext.js
@@ -3,60 +3,9 @@ import visionGameData from "@/components/Common/VisionGame/visionGameData";
 
 export const VisionGameContext = createContext();
 
-const testData = [
-  {
-    id: 1,
-    selectedAnswer: "incorrect",
-  },
-  {
-    id: 2,
-    selectedAnswer: "yes",
-  },
-  {
-    id: 3,
-    selectedAnswer: "no",
-  },
-  {
-    id: 4,
-    selectedAnswer: "no",
-  },
-  {
-    id: 5,
-    selectedAnswer: "yes",
-  },
-  {
-    id: 6,
-    selectedAnswer: "yes",
-  },
-  {
-    id: 7,
-    selectedAnswer: "yes",
-  },
-  {
-    id: 8,
-    selectedAnswer: "no",
-  },
-  {
-    id: 9,
-    selectedAnswer: "yes",
-  },
-  {
-    id: 10,
-    selectedAnswer: "no",
-  },
-  {
-    id: 11,
-    selectedAnswer: "no",
-  },
-  {
-    id: 12,
-    selectedAnswer: "yes",
-  },
-];
-
 export const VisionGameProvider = ({ children }) => {
   // 小遊戲一玩家答案，預設為空陣列
-  const [playerAnswers, setPlayerAnswers] = useState(testData);
+  const [playerAnswers, setPlayerAnswers] = useState([]);
 
   return (
     <VisionGameContext.Provider
