7dddce98c1519470b62ccae136f81668a36a212c
Kristy Liu <kristy.liu@plantsist.com>
2024-09-26 10:51:04 +0800

    feat: update ga4

--- pages/_app.js
+++ pages/_app.js
@@ -23,11 +23,14 @@ export default function App({ Component, pageProps }) {
       });
     };
 
+    // 初始頁面加載時發送事件
+    handleRouteChange(router.pathname);
+
     router.events.on("routeChangeComplete", handleRouteChange);
     return () => {
       router.events.off("routeChangeComplete", handleRouteChange);
     };
-  }, [router.events]);
+  }, [router.events, router.pathname]);
 
   return (
     <>
