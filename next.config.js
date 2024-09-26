2d69c4c02d1811a2e036d4148f588b5d2099d9da
Kristy Liu <kristy.liu@plantsist.com>
2024-09-26 11:34:17 +0800

    feat: update config

--- next.config.js
+++ next.config.js
@@ -1,11 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // output: "export",
-  // images: {
-  //   loader: "custom",
-  //   loaderFile: "./imageLoader.js",
-  // },
+  output: "export",
+  trailingSlash: true,
+  images: {
+    loader: "custom",
+    loaderFile: "./imageLoader.js",
+  },
 };
 
 module.exports = nextConfig;
