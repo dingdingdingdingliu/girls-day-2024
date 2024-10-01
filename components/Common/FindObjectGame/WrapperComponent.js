709c6adbf3547c4dbd0dd534e893b25e0aa52944
Kristy Liu <kristy.liu@plantsist.com>
2024-10-01 22:08:03 +0800

    Revert "fix: fix dialog scroll style"
    This reverts commit 73a8a525dbfece57c35adb14600c880591f070af.

--- components/Common/FindObjectGame/WrapperComponent.js
+++ components/Common/FindObjectGame/WrapperComponent.js
@@ -69,6 +69,8 @@ export const AnimatedDialogWrapper = styled(animated.div)`
   border-radius: 2px;
   position: relative;
   padding-top: 32px;
+  overflow-y: auto;
+  overflow-x: hidden;
 
   @media (max-width: ${globalConfig.mediaQuery}) {
     width: 92%;
@@ -84,6 +86,4 @@ export const ContentWrapper = styled.div`
   flex-direction: column;
   justify-content: start;
   align-items: center;
-  overflow-y: auto;
-  overflow-x: hidden;
 `;
